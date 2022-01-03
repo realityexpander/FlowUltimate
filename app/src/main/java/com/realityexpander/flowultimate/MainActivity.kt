edbd1613928d2d8c3adf1dcb860be07b42e189c7
Chris Athanas <chris@crowdskout.com>
2022-01-03 01:02:18 -0600

    Add stateFlow and sharedFlow

--- app/src/main/java/com/realityexpander/flowultimate/MainActivity.kt
+++ app/src/main/java/com/realityexpander/flowultimate/MainActivity.kt
@@ -1,35 +1,67 @@
 package com.realityexpander.flowultimate
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.realityexpander.flowultimate.databinding.ActivityMainBinding
 import com.realityexpander.flowultimate.ui.theme.FlowUltimateTheme
+import kotlinx.coroutines.flow.collect
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() { // for Compose
+//class MainActivity : AppCompatActivity() {  // for XML
+//    private lateinit var bind: ActivityMainBinding // for XML
+//    private val viewModel: MainViewModel by viewModels() // for XML
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        // For XML apps
+//        bind = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(bind.root)
+//        collectLatestLifecycleFlow(viewModel.stateFlow) { number ->
+//            bind.counterTv.text = number.toString()
+//        }
+
         setContent {
             FlowUltimateTheme {
                 val viewModel = viewModel<MainViewModel>()
                 val time = viewModel.countdownFlow.collectAsState(initial = 10)
+                val count = viewModel.stateFlow.collectAsState(initial = 0)
+
+                LaunchedEffect(key1 = true) {
+                    viewModel.sharedFlow.collect { number ->
+                        Toast.makeText(applicationContext, "The number is $number", Toast.LENGTH_SHORT ).show()
+                    }
+                }
+                
                 Box( modifier = Modifier.fillMaxSize(1f)) {
                     Text(
                         text = time.value.toString(),
                         fontSize = 30.sp,
                         modifier = Modifier.align(Alignment.Center)
                     )
+
+                    Button(onClick = { viewModel.incrementCounter() }) {
+                        Text(text = "Counter: ${count.value}")
+                    }
                 }
             }
         }
@@ -48,3 +80,23 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+// // collect for StateFlows
+//fun <T> AppCompatActivity.collectLatestLifecycleFlow(flow: Flow<T>, suspend (T) -> Unit) { // For XML projects
+//fun <T> ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>, suspend (T) -> Unit) {
+//    lifecycleScope.launch {
+//            repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                flow.collectLatest(collect)
+//            }
+//        }
+//}
+
+// // Collect for sharedFlows
+//fun <T> AppCompatActivity.collectLifecycleFlow(flow: Flow<T>, suspend (T) -> Unit) { // For XML projects
+//fun <T> ComponentActivity.collectLifecycleFlow(flow: Flow<T>, suspend (T) -> Unit) {
+//    lifecycleScope.launch {
+//            repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                flow.collect(collect)
+//            }
+//        }
+//}
--- app/src/main/java/com/realityexpander/flowultimate/MainViewModel.kt
+++ app/src/main/java/com/realityexpander/flowultimate/MainViewModel.kt
@@ -1,12 +1,12 @@
 package com.realityexpander.flowultimate
 
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.realityexpander.flowultimate.MainViewModel.Companion.outString
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -17,7 +17,7 @@ class MainViewModel: ViewModel() {
     }
 
     val countdownFlow = flow<Int> {
-        val startingValue = 100
+        val startingValue = 5
         var currentValue = startingValue
 
         emit(startingValue)
@@ -28,15 +28,76 @@ class MainViewModel: ViewModel() {
         }
     }
 
+    private val _stateFlow = MutableStateFlow(0)
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<Int>(0) // Hot
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    private val _sharedFlowReplayed = MutableSharedFlow<Int>(5) // Hot & replays to new collectors
+    val sharedFlowReplayed = _sharedFlowReplayed.asSharedFlow()
+
     init {
-        collectFlow()
-        collectFlowLatest()
+//        collectFlow()
+//        collectFlowLatest()
+//        collectFlowFiltered()
+//        collectFlattenFlows()
+//        collectFlattenRecipesFlow()
+//        collectMealsFlow()
+
+
+        // Sharedflow emitters
+        //squaredNumber(10) // this will be lost because sharedFlow is a hot flow, and this will be lost!
+        viewModelScope.launch {
+            sharedFlow.collect {
+                delay(1000)
+                println("FIRST FLOW: The received number is $it")
+            }
+        }
+        viewModelScope.launch {
+            sharedFlow.collect {
+                delay(1500)
+                println("SECOND FLOW: The received number is $it")
+            }
+        }
+        squaredNumber(10)
+
+
+        // ShareFlowReplayed
+        squaredNumberReplayed(10)  // this will be flowed bc this is using a replay > 0
+        viewModelScope.launch {
+            sharedFlowReplayed.collect {
+                delay(1500)
+                println("REPLAYED FLOW: The received number is $it")
+            }
+        }
+
+        // Change the sharedFlow after the viewmodel is setup
+        viewModelScope.launch {
+            _sharedFlow.emit(999)
+        }
+
+    }
+
+    fun incrementCounter() {
+        _stateFlow.value += 1
+    }
+
+    private fun squaredNumber(number: Int) {
+        viewModelScope.launch {
+            _sharedFlow.emit(number * number)
+        }
+    }
+
+    private fun squaredNumberReplayed(number: Int) {
+        viewModelScope.launch {
+            _sharedFlowReplayed.emit(number * number)
+        }
     }
 
     private fun collectFlow() {
         viewModelScope.launch {
             countdownFlow.collect { time ->
-//                println("The current time is $time")
                 outString = "The current time is $time"
                 println(outString)
             }
@@ -46,9 +107,181 @@ class MainViewModel: ViewModel() {
     private fun collectFlowLatest() {
         viewModelScope.launch {
             countdownFlow.collectLatest { time ->
-                delay(1002L)
+                delay(1002L) // cancels when next emission happens in countdownFlow
                 outString += (", The latest time is $time")
                 println(outString)
+            }
+        }
+    }
+
+    private fun collectFlowFiltered() {
+
+        // alternate way of doing launch
+        countdownFlow.onEach { time ->
+            println("from alternate launch time: $time")
+        }.launchIn(viewModelScope)
+
+        // filter, map, onEach
+        viewModelScope.launch {
+            countdownFlow
+                .filter { time ->
+                    time %2 == 0
+                }
+                .map { time ->
+                    time * 2
+                }
+                .onEach { time ->
+                    println(time)
+                }
+                .collect { time ->
+                    outString = "The filtered time is $time"
+                    println(outString)
+                }
+        }
+
+        // count
+        viewModelScope.launch {
+            val countEvens = countdownFlow.count { time ->
+                println("counting time= $time")
+                time % 2 == 0
+            }
+            println("time - count of Evens: $countEvens") // will wait for the countdownFlow to complete before hitting this line
+        }
+
+        // reduce
+        viewModelScope.launch {
+            val reduceResult = countdownFlow
+                .reduce { acc, time ->
+                    acc + time
+                }
+            println("time - the reduce acc = $reduceResult") // will wait for the countdownFlow to complete before hitting this line
+        }
+
+        // fold
+        viewModelScope.launch {
+            val foldResult = countdownFlow
+                .fold(1000) { acc, time ->
+                    acc + time
+                }
+            println("time - the fold acc = $foldResult") // will wait for the countdownFlow to complete before hitting this line
+        }
+    }
+
+    private fun collectFlattenFlows() {
+        val flow1 = flow {
+            emit(1)
+            delay(500)
+            emit(2)
+        }
+
+        viewModelScope.launch {
+            flow1.flatMapConcat { value -> // each time this emits...
+                flow {                          // ... this flow emits
+                    emit(10 + value)
+                    delay(400)
+                    emit(20 + value)
+                    delay(600)
+                    emit(30 + value)
+                }
+            }.collect { value ->
+                println("the value is $value")
+            }
+        }
+    }
+
+    private fun collectFlattenRecipesFlow() {
+
+        val recipeIds = (1..5).asFlow()
+
+        fun getRecipeById(id: Int) = flow {
+            emit("Recipe $id")
+            delay(1000)
+        }
+
+        viewModelScope.launch {
+            recipeIds.flatMapConcat { id -> // respects the delay
+                delay(200)
+                getRecipeById(id)  // by using flatMapConcat, we are now returning the values of the flow, and not just the flow
+            }.collect { value ->
+                println("the flatMapConcat value is $value")
+            }
+        }
+
+        viewModelScope.launch {
+            recipeIds.flatMapMerge { id ->  // flatMapMerge does not use "delay" to receive values, (not recommended for apps)
+                delay(200)
+                getRecipeById(id)  // by using flatMapMerge, we are now returning the values of the flow, and not just the flow
+            }.collect { value ->
+                println("the flatMapMerge value is $value")
+            }
+        }
+
+        viewModelScope.launch {
+            recipeIds.flatMapLatest { id ->  // flatMapLatest will cancel if there are new values emitted
+                delay(200)
+                getRecipeById(id)  // by using flatMapLatest, we are now returning the values of the flow, and not just the flow
+            }.collect { value ->
+                println("the flatMapLatest value is $value")
+            }
+        }
+    }
+
+    private fun collectMealsFlow() {
+
+        val flow = flow {
+            delay(250)
+            emit("Appetizer 1")
+            delay(1000)
+            emit("Main Dish 2")
+            delay(100)
+            emit("Dessert   3")
+        }
+
+
+        viewModelScope.launch {
+
+            // Unbuffered (sequential)
+            flow.onEach {
+                println("MEAL:A Delivered $it")
+            }.collect {
+                println("MEAL:B Eating    $it")
+                delay(1500)
+                println("MEAL:C Finished  $it")
+            }
+
+            // Buffered - collect flow in a seperate coroutine
+            println("\n\nMEAL: Buffered:")
+            flow.onEach {
+                println("MEAL:A Delivered $it")
+            }
+            .buffer()  // collects in separate coroutine, doesn't wait for emitter
+            .collect {
+                println("MEAL:B Eating    $it")
+                delay(1500)
+                println("MEAL:C Finished  $it")
+            }
+
+            // Conflated - cancels flow if new emission
+            println("\n\nMEAL: Conflate:")
+            flow.onEach {
+                println("MEAL:A Delivered $it")
+            }
+            .conflate()  // runs in separate coroutine, cancels the collect if new emit arrives
+            .collect {
+                println("MEAL:B Eating    $it")
+                delay(1500)
+                println("MEAL:C Finished  $it")
+            }
+
+            // CollectLatest - cancels collect if new emit, starts with latest emit
+            println("\n\nMEAL: CollectLatest:")
+            flow.onEach {
+                println("MEAL:A Delivered $it")
+            }
+            .collectLatest {
+                println("MEAL:B Eating    $it")
+                delay(1500)
+                println("MEAL:C Finished  $it")
             }
         }
     }
